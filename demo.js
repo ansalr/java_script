785273fe804548f8016c204e440130658c4e9be6
ansalr <ansalr.email@gmail.com>
2024-01-15 16:37:31 +0530

    day 2 update

---
+++ demo.js
@@ -0,0 +1,41 @@
+var a = Math.random()
+
+console.log(Math.floor(a*10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var col = "green"
+
+// if (col === "red"){
+//     console.log("stop");
+// }else if (col === "yellow"){
+//     console.log("ready");
+// }else{
+// console.log("Go");
+// }
